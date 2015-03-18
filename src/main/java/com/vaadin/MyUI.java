2a58ebd67107a4f5b1fd76d5c82c2a8583969660
Guillermo Alvarez <guillermo@vaadin.com>
2015-03-18 20:17:25 +0200

    Add new list with uploaded files

--- src/main/java/com/vaadin/MyUI.java
+++ src/main/java/com/vaadin/MyUI.java
@@ -1,10 +1,15 @@
 package com.vaadin;
 
+import static com.vaadin.data.util.FilesystemContainer.PROPERTY_NAME;
+import static com.vaadin.server.FontAwesome.FILE_EXCEL_O;
+import static com.vaadin.server.Sizeable.Unit.PERCENTAGE;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,14 +20,19 @@ import com.vaadin.addon.spreadsheet.Spreadsheet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.data.util.FilesystemContainer;
 import com.vaadin.server.FileDownloader;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.StreamResource;
 import com.vaadin.server.StreamResource.StreamSource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
-import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.HorizontalSplitPanel;
+import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Upload;
 import com.vaadin.ui.Upload.FinishedEvent;
@@ -39,66 +49,98 @@ public class MyUI extends UI {
 
     private static final long serialVersionUID = -5813234435312189521L;
 
-    private File tempFile;
     private Spreadsheet spreadsheet;
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        layout.setSpacing(true);
-        layout.setSizeFull();
-        setContent(layout);
+        HorizontalSplitPanel horizontalSplitPanel = new HorizontalSplitPanel();
+        horizontalSplitPanel.setSplitPosition(300, Unit.PIXELS);
+        setContent(horizontalSplitPanel);
 
         initSpreadsheet();
 
-        layout.addComponent(createToolbar());
-        layout.addComponent(spreadsheet);
-        layout.setExpandRatio(spreadsheet, 1);
+        horizontalSplitPanel.setFirstComponent(createToolbar());
+        horizontalSplitPanel.setSecondComponent(spreadsheet);
     }
 
     private Component createToolbar() {
-        HorizontalLayout toolbar = new HorizontalLayout();
+        final File templatesDirectory = new File("sheets" + File.separator);
+        if (!templatesDirectory.exists()) {
+            templatesDirectory.mkdir();
+        }
+        VerticalLayout toolbar = new VerticalLayout();
+        toolbar.setSizeFull();
         toolbar.setSpacing(true);
 
+        FilesystemContainer filesContainer = new FilesystemContainer(
+                templatesDirectory);
+        filesContainer.setRecursive(false);
+        filesContainer.setFilter(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                if (name != null
+                        && (name.endsWith(".xls") || name.endsWith(".xlsx") || name
+                                .endsWith(".xlsm"))) {
+                    return true;
+                } else {
+                    File toDelete = new File(dir, name);
+                    toDelete.delete();
+                    return false;
+                }
+            }
+        });
+
+        final ListSelect availableFiles = new ListSelect("Available templates",
+                filesContainer);
+        availableFiles.setSizeFull();
+        availableFiles.setIcon(FILE_EXCEL_O);
+        availableFiles.setImmediate(true);
+        availableFiles.setNullSelectionAllowed(false);
+        availableFiles.setItemCaptionPropertyId(PROPERTY_NAME);
+        availableFiles.addValueChangeListener(new ValueChangeListener() {
+            private static final long serialVersionUID = 8048738040109480947L;
+
+            @Override
+            public void valueChange(ValueChangeEvent event) {
+                Object value = availableFiles.getValue();
+                if (value != null && value instanceof File) {
+                    loadFile((File) value);
+                }
+            }
+        });
+
         final Upload upload = new Upload();
+        upload.setWidth(100, PERCENTAGE);
+        upload.setButtonCaption("Add new template");
+        upload.setImmediate(true);
         upload.setReceiver(new Receiver() {
+            private static final long serialVersionUID = -2293616343861518740L;
 
             @Override
             public OutputStream receiveUpload(String filename, String mimeType) {
                 try {
-                    tempFile = new File(filename);
-                    tempFile.deleteOnExit();
-
-                    FileOutputStream fos = new FileOutputStream(tempFile);
-                    return fos;
+                    File tempFile = new File(templatesDirectory, filename);
+                    return new FileOutputStream(tempFile);
                 } catch (FileNotFoundException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-
                 return null;
             }
         });
         upload.addFinishedListener(new FinishedListener() {
+            private static final long serialVersionUID = -8840790897095932946L;
 
             @Override
             public void uploadFinished(FinishedEvent event) {
-                if (tempFile != null) {
-                    try {
-                        spreadsheet.read(tempFile);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        tempFile.delete();
-                    }
-                }
+                availableFiles.markAsDirty();
             }
         });
-        upload.setImmediate(true);
-        Button download = new Button("Download");
+        final Button download = new Button("Download edited");
+        download.setWidth(100, PERCENTAGE);
+        download.setIcon(FontAwesome.DOWNLOAD);
         FileDownloader fileDownloader = new FileDownloader(new StreamResource(
                 new StreamSource() {
+                    private static final long serialVersionUID = 2076495798023012455L;
 
                     @Override
                     public InputStream getStream() {
@@ -118,8 +160,19 @@ public class MyUI extends UI {
         fileDownloader.extend(download);
 
         toolbar.addComponent(upload);
+        toolbar.addComponent(availableFiles);
         toolbar.addComponent(download);
+        toolbar.setExpandRatio(availableFiles, 1);
+
         return toolbar;
+    }
+
+    private void loadFile(File file) {
+        try {
+            spreadsheet.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void initSpreadsheet() {
